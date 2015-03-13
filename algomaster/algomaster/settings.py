945a2954eea00b1a1814a9846b33c4ebdb4a9568
Shashi Essp <shashiessp@gmail.com>
2015-03-14 01:18:26 +0530

    Heroku config

--- algomaster/algomaster/settings.py
+++ algomaster/algomaster/settings.py
@@ -83,3 +83,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
