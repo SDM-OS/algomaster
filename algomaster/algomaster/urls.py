82d4a07a6ef1cb7586a832709b7ca22aa72e5367
Shashi Essp <shashiessp@gmail.com>
2015-03-14 07:12:55 +0530

    Fix Urls

--- algomaster/algomaster/urls.py
+++ algomaster/algomaster/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import content.views as views
+from content.views import AlgoListView, AlgoDetailed 
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'algomaster.views.home', name='home'),
@@ -8,5 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^$', views.landing,),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^algorithms/', include('content.urls')),
+    url(r'^explore/', AlgoListView.as_view()),
+    url(r'^explore/(?P<pk>[-_\w]+)/$', AlgoDetailed.as_view()),
+    # url(r'^algorithms/', include('content.urls')),
 )
